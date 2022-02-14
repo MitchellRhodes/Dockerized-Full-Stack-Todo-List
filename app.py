41229fa14bbd67cfbe52712f1c611a8176083a5a
Mitch Rhodes <M.Rhodes87@comcast.net>
2022-02-13 21:43:22 -0500

    Changed to sqlite for simplicity. Tasks get added to page.

--- app.py
+++ app.py
@@ -2,18 +2,35 @@ from flask import Flask, redirect, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-import os
-
-username = os.environ['PGUSER']
-password = os.environ['PGPASSWORD']
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://PGPASSWORD@PGUSER/flaskTodoList'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskTodoList.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String , nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=[ 'GET', 'POST'])
+
 def index():
-    return render_template('index.html')
-    
+    if request.method == 'POST':
+        task_content = request.form['content']
+        newTask = Todo(content=task_content)
+
+        try:
+            db.session.add(newTask)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your Todo."
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
 if __name__ == "__main__":
     app.run(debug=True)
