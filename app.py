d8c8f4883b359681471bc41c3f274d39a88fe4a3
Mitch Rhodes <M.Rhodes87@comcast.net>
2022-02-13 21:15:00 -0500

    Set up base and index htmls, and added basic css.

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from flask import Flask, redirect, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+import os
+
+username = os.environ['PGUSER']
+password = os.environ['PGPASSWORD']
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://PGPASSWORD@PGUSER/flaskTodoList'
 db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
